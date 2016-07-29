28f26dbc301283efec4b0b36b01404b240ed12d5
Skemp45 <Shawn@besmartdesigns.com>
2016-07-28 22:47:43 -0700

    Initial Commit
    Initial code commit

---
+++ PintsCall.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Plugin Name: Raspberry Pints Tap Access
+ * Description: This plugin will use your API Key and Secret to pull in your active taplist onto your website.
+ * Version: 1.0
+ * Author: Six Pack Apps
+ * Author URI: http://raspberrypints.com
+ * License: GPL2
+ */
+
+/*  Copyright 2016  Six Pack Apps  (email : dev@sixpackapps.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+**************************************************************************/
+
+// Blocks direct access to plugin
+defined( 'ABSPATH' ) or die( "Access Forbidden" );
+
+// Define Second Street Plugin
+define( 'WPRPINTS_PLUGIN_VERSION', '1.0' );
+define( 'WPRPINTS_PLUGIN__MINIMUM_WP_VERSION', '4.5' );
+define( 'WPRPINTS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'WPRPINTS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+
+// [ss-promo] Code
+function validationCall( $creds, $content = null ) {
+	$RP = shortcode_creds( array (
+			'API_KEY' => '',
+			'API_Secret' => '',
+			'Page' => ''
+		), $creds );
+
+	$RP_script_url = 'http://raspberrypints.com/' . $RP['Page'] . '/?'. $RP['API_KEY']  .': '. $RP['API_Secret']  .'';
+
+	return "<iframe style='width: 100%; height: 1000px; border: none; outline: none;' src='' width='300' height='150'></iframe>";
+
+}
+add_shortcode( 'RPints', 'validationCall' );
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # WP-RPints
 Raspberry Pints Plugin
+This plugin pulls in your active taplist into your website.
+Benefits
+- Active Taplist on site
+- Easy shortcode inclusion
