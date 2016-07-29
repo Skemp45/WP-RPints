33bc9080ad34ff95f8587260e56a41690a111d9c
Skemp45 <Shawn@besmartdesigns.com>
2016-07-29 15:06:22 -0700

    Database from ui update
    AAdded function to create database and to sync data from ui to DB

--- PintsCall.php
+++ PintsCall.php
@@ -32,6 +32,72 @@ define( 'WPRPINTS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 // Blocks direct access to plugin
 defined( 'ABSPATH' ) or die( "Access Forbidden" );
 
+/////////////////////////////////////////////////////////////
+//              Database Related Functions                 //
+/////////////////////////////////////////////////////////////
+
+//Table Creation
+function RPDB_install () {
+   global $wpdb;
+
+   //define table name
+   $table_name = $wpdb->prefix . "raspberrypints";
+   $charset_collate = $wpdb->get_charset_collate();
+
+   //sql statement to create table
+   $sql = "CREATE TABLE $table_name (
+      id int(255) NOT NULL AUTO_INCREMENT,
+      api_key varchar(255) NOT NULL,
+      api_secret varchar(255) NOT NULL,
+      UNIQUE KEY id (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+}
+
+
+// //insert data into Table
+function RPDB_insert_data($apiK, $apiS) {
+	global $wpdb;
+	$table_name = $wpdb->prefix . 'raspberrypints';
+  $results = $wpdb->get_row( "SELECT id FROM ". $table_name." LIMIT 1" );
+  $apiId = $results->id;
+  if($apiId < 1){
+    $wpdb->insert(
+  		$table_name,
+  		array(
+  			'api_key' => $apiK,
+  			'api_secret' => $apiS
+  		)
+  	);
+  }
+  else
+  {
+  $wpdb->update(
+    $table_name,
+    array(
+      'api_key' => $apiK,
+      'api_secret' => $apiS
+    ),
+    array(
+      'id' => 1
+    )
+  );
+  }
+
+  register_activation_hook( __FILE__, 'RPDB_insert_data' );
+  echo "<script>window.location.reload()</script>";
+}
+
+
+//Install db settings on install
+register_activation_hook( __FILE__, 'RPDB_install' );
+
+
+/////////////////////////////////////////////////////////////
+//           Start Admin Menu function section             //
+/////////////////////////////////////////////////////////////
 //Build out Admin UI
 add_action('admin_menu', 'WPRP_menu');
 
@@ -50,10 +116,24 @@ function WPRP_menu(){
 
 //Create Display Inside admin section
 function wprp_init(){
+  global $wpdb;
+
+  $table_name = $wpdb->prefix . 'raspberrypints';
+  $results = $wpdb->get_row( "SELECT id, api_key, api_secret FROM ". $table_name ." WHERE id = 1" );
+
+  $apiKey = $results->api_key;
+  $apiSecret = $results->api_secret;
+
+  if (isset($_POST["apiKey"])){
+    $apiK = $_POST["apiKey"];
+    $apiS = $_POST["apiSecret"];
+    RPDB_insert_data($apiK, $apiS);
+
+  }
 
   //Title Section
   echo "<h1>RaspberryPints Taplist Admin</h1>";
- 
+
   //set navigation
   echo "<h2 class='nav-tab-wrapper'><a class='nav-tab nav-tab-active' id='api-tab' href='#top#api'>API Settings</a><a class='nav-tab nav-tab-disabled' id='other-tab' href='#top#other'>Other</a></h2>";
 
@@ -63,16 +143,22 @@ function wprp_init(){
   </p>";
 
   //Content divs
-  echo"<div id='api'>
+  echo "<div id='api'>
+          <form action='' method='POST'>
           <label><b>API Key:</b></label><br/>
-              <input type='text' name='apiKey' /><br/>
+              <input type='text' name='apiKey' value='".$apiKey."'/><br/>
           <label><b>API Secret:</b></label><br/>
-              <input type='text' name='apiSecret' /><br/><br/>
+              <input type='text' name='apiSecret' value='".$apiSecret."'/><br/><br/>
               <button class='button button-primary'>Submit</button>
+          </form>
       </div>";
-  echo"<div id='other'></div>";
+  echo "<div id='other'></div>";
 
-    }
+  }
+/////////////////////////////////////////////////////////////
+//            End Admin Menu function section              //
+/////////////////////////////////////////////////////////////
+
 
 // [rpints] Code
 function validationCall( $creds, $content = null ) {
